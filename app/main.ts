7d66434f5c556537765f34f1f00a95beed9f0a73
Kazutaka Tonsho <Kazutaka.Tonsho@gmail.com>
2017-02-12 11:33:14 +0900

    Introduce todoList directive

--- app/main.ts
+++ app/main.ts
@@ -16,35 +16,6 @@ app.controller('todoController', ['$scope', ($scope) => {
         index++;
     };
 
-    // todoItem を更新
-    $scope.updateTodoItem = (todoItem => {
-        var message = window.prompt('変更', todoItem.message);
-        if (message) {
-            var t;
-            for (var i = 0; i < $scope.todoItems.length; i++) {
-                t = $scope.todoItems[i];
-                if (t.id == todoItem.id) {
-                    t.message = message;
-                    break;
-                }
-            }
-        }
-    });
-
-    // todoItem を削除
-    $scope.removeTodoItem = (id) => {
-        var index = 1;
-        var t;
-        for (var i = 0; i < $scope.todoItems.length; i++) {
-            t = $scope.todoItems[i];
-            if (t.id == id) {
-                index = i;
-                break;
-            }
-        }
-        $scope.todoItems.splice(index, 1);
-    };
-
     // 完了アイテム数を取得
     $scope.remaining = () => {
         var count = 0;
@@ -54,3 +25,57 @@ app.controller('todoController', ['$scope', ($scope) => {
         return count;
     };
 }]);
+
+app.directive('todoList', [() => {
+    interface ImyScope extends ng.IScope {
+        todoItems: any;
+        update: (x: any, y: any) => void;
+        delete: (x: any, y: any) => void;
+    }
+    return {
+        restrict: 'EA',
+        replace: true,
+        template:
+            '<ul class="list-group">' +
+                '<li class="list-group-item" ng-repeat="todoItem in todoItems">' +
+                    '<div class="list-group-item-inner">' +
+                        '<div class="item-wrapper"><input type="checkbox" ng-model="todoItem.done"></div>' +
+                        '<label class="done-{{todoItem.done}}" ng-dblclick="update($event, todoItem)">{{todoItem.message}}</label>' +
+                        '<div class="item-wrapper">' +
+                            '<button class="btn btn-xs btn-danger" ng-click="delete($event, todoItem.id)">&times;</button>' +
+                        '</div>' +
+                    '</div>' +
+                '</li>' +
+            '</ul>',
+        link: (scope: ImyScope, iElement) => {
+            // todoItem を更新
+            scope.update = ($event, todoItem) => {
+                var message = window.prompt('変更', todoItem.message);
+                if (message) {
+                    var t;
+                    for (var i = 0; i < scope.todoItems.length; i++) {
+                        t = scope.todoItems[i];
+                        if (t.id == todoItem.id) {
+                            t.message = message;
+                            break;
+                        }
+                    }
+                }
+            };
+            // todoItem を削除
+            scope.delete = ($event, itemId) => {
+                var index = 1;
+                var t;
+                for (var i = 0; i < scope.todoItems.length; i++) {
+                    t = scope.todoItems[i];
+                    if (t.id == itemId) {
+                        index = i;
+                        break;
+                    }
+                }
+                scope.todoItems.splice(index, 1);
+            };
+        }
+    }
+}]);
+

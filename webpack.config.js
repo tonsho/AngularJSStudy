73e5dac1356200332cc6021dd4acd3eda127d721
Kazutaka Tonsho <Kazutaka.Tonsho@gmail.com>
2017-02-19 00:03:26 +0900

    Build ts files by ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-    entry: './app/main.js',
+    entry: './app/Main.ts',
     output: {
         path: 'build',
         filename: 'bundle.js'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
     },
     externals: {
         "angular": "angular"
